46fe585c4ee2ef5f5ceb68b3e97afe7aaf44a927
Mary <mcrowe0331@gmail.com>
2023-01-24 23:35:19 -0600

    final

--- OOP.py
+++ OOP.py
@@ -28,5 +28,6 @@ class SebulbasPod(Podracer):
     
 # Make sure to answer the following prompts about your coding experience:
 # How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
+# Encapsulation, Abstraction
 # Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
 # How in particular did Object Oriented Programming assist in the solving of this problem?##
